75adb8b7cb3f47f5045e0aaeb5a7f05c71cdd80d
Iulia Groza <iuliag523@gmail.com>
2024-01-06 16:48:30 +0200

    Revert

--- Lab5/budget_planner_server/income/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Income(models.Model):
-    amount = models.FloatField()
-    date = models.DateField()
-    category = models.CharField(max_length=100)
-    note = models.TextField()
